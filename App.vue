20547e39feaf2e6a5e396f3eb41853bf4d461e32
ktsrkw <53892437+ktsrkw@users.noreply.github.com>
2021-01-20 20:08:36 +0800

    将本地项目源文件移植到repositroy

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	// import uniPopup from "components/uni-popup/uni-popup.vue"
+	export default { 
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+		// components:{
+		// 	uniPopup
+		// }
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
